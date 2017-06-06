594f91e3902e4e0e2b1e0dd498bf73f56c0da7fd
underqiu <underqiu@outlook.com>
2017-06-06 23:19:43 +0800

    lets dance

---
+++ opengles1Tests/opengles1Tests.m
@@ -0,0 +1,39 @@
+//
+//  opengles1Tests.m
+//  opengles1Tests
+//
+//  Created by qiu on 05/06/2017.
+//  Copyright © 2017 qiu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface opengles1Tests : XCTestCase
+
+@end
+
+@implementation opengles1Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
